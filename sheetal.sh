66857a8188a1d5212e87f1839b9dab3be0cd1775
SheetaI <im.kawaii.gg@gmail.com>
2022-04-23 23:23:32 +0800

    Update sheetal.sh

--- sheetal.sh
+++ sheetal.sh
@@ -142,8 +142,10 @@ sudo groupadd -r autologin
 sudo gpasswd -a sheetal autologin
 sudo sed -i "s/^#autologin-user=$/autologin-user=sheetal/" /etc/lightdm/lightdm.conf
 sudo sed -i "s/^#autologin-user-timeout=0$/autologin-user-timeout=0/" /etc/lightdm/lightdm.conf
+clear
 
 echo "-------------------------------------------------------------------"
-echo "Finished Installing Everything...Reboot & Enjoy !!!"
+echo "Personalization Finished... Reboot & Enjoy !!!"
 echo "-------------------------------------------------------------------"
+sleep 3
 exit
